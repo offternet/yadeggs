27c22ce5b53079651da58618c32cdd36acbd15a9
Robert Cooper <68208919+offternet@users.noreply.github.com>
2023-10-14 11:18:45 -0700

    Update yadeggs-settings-display.sh
    Clanged yad-eggs6 to yad

--- config/yadeggs-settings-display.sh
+++ config/yadeggs-settings-display.sh
@@ -2,38 +2,36 @@
 
 KEY=$RANDOM
 
-sleep 15 &
-
-yad-eggs6 --plug=$KEY --tabnum=1 \
+yad --plug=$KEY --tabnum=1 \
 --text="/etc/penguins-eggs.d/eggs.yaml"  \
 --text-info --filename="/etc/penguins-eggs.d/eggs.yaml" --wrap --show-uri &
 
-yad-eggs6 --plug=$KEY --tabnum=2 \
+yad --plug=$KEY --tabnum=2 \
 --text="/etc/penguins-eggs.d/krill.yaml" \
 --text-info --filename="/etc/penguins-eggs.d/krill.yaml" --wrap --show-uri &
 
-yad-eggs6 --plug=$KEY --tabnum=3 \
+yad --plug=$KEY --tabnum=3 \
 --text="/etc/penguins-eggs.d/derivatives.yaml" \
 --text-info --filename="/etc/penguins-eggs.d/derivatives.yaml" --wrap --show-uri &
 
-yad-eggs6 --plug=$KEY --tabnum=4 \
+yad --plug=$KEY --tabnum=4 \
 --text="/etc/penguins-eggs.d/archiso.yaml" \
 --text-info --filename="/etc/penguins-eggs.d/archiso.yaml" --wrap --show-uri &
 
-yad-eggs6 --plug=$KEY --tabnum=5  \
+yad --plug=$KEY --tabnum=5  \
 --text="/etc/penguins-eggs.d/tools.yaml"   \
 --text-info --filename="/etc/penguins-eggs.d/tools.yaml" --wrap --show-uri &
 
 
-yad-eggs6 --plug=$KEY --tabnum=6  \
+yad --plug=$KEY --tabnum=6  \
 --text="/usr/local/share/penguins-eggs/exclude.list"   \
 --text-info --filename="/usr/local/share/penguins-eggs/exclude.list" --wrap --show-uri &
 
-yad-eggs6 --plug=$KEY --tabnum=7  \
+yad --plug=$KEY --tabnum=7  \
 --text="/etc/penguins-eggs.d/README.md/etc/"   \
 --text-info --filename="/etc/penguins-eggs.d/README.md" --wrap --show-uri &
 
-yad-eggs6  --key=$KEY --notebook --tab-pos="top"   \
+yad --key=$KEY --notebook --tab-pos="top"   \
  --geometry="+1900+0" --width=250 --height=570 --no-buttons \
 --title="penguins-eggs Settings" \
 --tab="eggs" \
